cc07aa0094806adfb35cc7b3b14e35455a17364d
bluevhxm5 <bluevhxm5@gmail.com>
2019-10-15 23:20:55 +0900

    토스트 및 alertDialog 처리

--- src/main/java/com/example/gangssam_ex_project/MainActivity.java
+++ src/main/java/com/example/gangssam_ex_project/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.gangssam_ex_project;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.Vibrator;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Vibrator;
+//import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public Context context=this;
 View.OnClickListener onclick=new View.OnClickListener() {
     @Override
     public void onClick(View v) {
@@ -30,6 +36,26 @@ View.OnClickListener onclick=new View.OnClickListener() {
                 //저장된 음악 재생
                 MediaPlayer player=MediaPlayer.create(getApplicationContext(),R.raw.weaponsound9);
                 player.start();
+                break;
+            case R.id.btn4:
+                //토스트 실행
+                Toast.makeText(getApplicationContext(),"테스트입니다.",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btn5:
+                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("d알람")
+                        .setMessage("정말 종료 하시겟습니까?")
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this,"button1111",Toast.LENGTH_LONG).show();
+                            }
+                        })
+                        .setNegativeButton("no",null);
+                AlertDialog alertDialog=builder.create();
+                alertDialog.show();
+
                 break;
         }
     }
@@ -51,9 +77,13 @@ View.OnClickListener onclick=new View.OnClickListener() {
         Button btn1=(Button)findViewById(R.id.btn1);
         Button btn2=(Button)findViewById(R.id.btn2);
         Button btn3=(Button)findViewById(R.id.btn3);
+        Button btn4=(Button)findViewById(R.id.btn4);
+        Button btn5=(Button)findViewById(R.id.btn5);
         btn1.setOnClickListener(onclick);
         btn2.setOnClickListener(onclick);
         btn3.setOnClickListener(onclick);
+        btn4.setOnClickListener(onclick);
+        btn5.setOnClickListener(onclick);
 
 
     }
